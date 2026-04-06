d50ec5260314ab639f5edd707e62e90c5e40cdf3
HusainAbbas257 <sayedhusainabbasrizvi@gmail.com>
2026-04-06 22:05:53 +0530

    refactor vector and ball classes: enhance documentation, improve initialization, and add docstrings to each funtion and added comments

--- physics/__main__.py
+++ physics/__main__.py
@@ -1,16 +1,16 @@
+# imports
 import math
 import pygame
 import random
 from vector import *
+
+# pygame setup-
 pygame.init()
 screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
 clock=pygame.time.Clock()
 width,height=screen.get_size()
 
-
-
 # helper funciton 
-
 def color(prev:tuple[int,int,int])->tuple[int,int,int]:
     color = pygame.Color(prev)
     hsva=(color.hsva)
@@ -19,61 +19,89 @@ def color(prev:tuple[int,int,int])->tuple[int,int,int]:
     color.hsva=(hue,hsva[1],100,hsva[3])
     return color.r,color.g,color.b
 
-
 # simulation thingys
 fps=60
 frame_count = 0
 g=acceleration(0,600)
 class Ball:
+    '''A ball class to implement all the basic properties of the ball'''
     def __init__(self):
-        self.pos=position(width//2,height//2)
-        self.vel=velocity(random.randint(-500,500),random.randint(-500,500))
-        self.color=(random.randint(0,255),random.randint(0,255),random.randint(0,255))  
+        '''initializes the ball with random velocity and color and mass based on radius'''
+        
+        self.pos=position(width//2,height//4) # start from the center top of the screen
+        
+        self.vel=velocity(random.randint(-500,500),random.randint(-500,500)) # random velocity in both x and y direction
+        self.color=(random.randint(0,255),random.randint(0,255),random.randint(0,255))  # random color for the ball later it gets the rainbow effect
+        
         # since mass logic is yet to be implemed make radius constant
         self.radius=15
-        self.rect=None
-        self.mass=(4/3)*math.pi*self.radius**3*0.0001 
+        
+        self.rect=None #stored rect thinking i would use it but found out that it is not needed but still keeping it for absoluely no reason
+        
+        self.mass=(4/3)*math.pi*self.radius**3*0.0001 #basic formula for mass of a sphere with density 0.0001 to make it look good
+        
         self.normal=False #wether a normal force acting on it or not
         
     def update(self,fps=fps):
-        self.color=color(self.color)
+        '''updates the position and velocity of the ball based on gravity and friction and also checks for collision with walls'''
         global g
-        self.vel*=0.99
+        
+        # change color for rainbow effect
+        self.color=color(self.color)
+        
+        self.vel*=0.99 # air resistance
+        
+        # gravity gets cancelled out by the normal force when the ball is on the ground so only apply gravity when it is not on the ground
         if not self.normal:
             self.vel+=g*(1/fps)
+        
+        # checking if its on ground
         if(self.pos.x<=self.radius*1.1 ): #a little more to take in the edge cases
             # apply friction
             self.normal=True
-            self.vel.x*=0.85
+            
+            self.vel.x*=0.85 # assuming friction only effects x direction for simplicity
         else:
             self.normal=False
+        
+        # distance=speed*time so:
         self.pos.x+=self.vel.x*(1/fps)
         self.pos.y+=self.vel.y*(1/fps)
-        if self.pos.x <= self.radius:
+        
+        # boundry collision:
+        if self.pos.x <= self.radius:# left wall
             self.pos.x = self.radius
             self.vel.x *= -0.9
-        elif self.pos.x >= width-self.radius:
+        elif self.pos.x >= width-self.radius:# right wall
             self.pos.x = width-self.radius
             self.vel.x *= -0.9
 
-        if self.pos.y <= self.radius:
+        if self.pos.y <= self.radius:# top wall
             self.pos.y = self.radius
             self.vel.y *= -0.9
-        elif self.pos.y >= height-self.radius:
+        elif self.pos.y >= height-self.radius:# bottom wall
             self.pos.y = height-self.radius
             self.vel.y *= -0.9
+        
+        # rounding off to avoid jitter
         self.vel.roundoff()
     def collision(self,other:'Ball'):
+        '''handles collision between two balls and also checks if they are on top of each other to apply normal force'''
+        
+        # distance between the centers of the two balls
         dx=self.pos.x-other.pos.x
         dy=self.pos.y-other.pos.y
         distance=math.sqrt(dx**2+dy**2)
-        distance=max(distance,0.01) #to avoid /0
-        if distance<self.radius+other.radius:
-            # normal vector
+        distance=max(distance,0.001) #to avoid /0
+        
+        if distance<self.radius+other.radius:#check for collison
+            
+            # normal vector 
             normal_vec=(other.pos-self.pos)/distance 
             # tangent vector
             tangent_vec=normal_vec.tangent()
             
+            # decomposing the velocity of both balls into normal and tangent components
             normal_component_self=self.vel.dot(normal_vec)
             normal_component_other=other.vel.dot(normal_vec)
             tangent_component_self=self.vel.dot(tangent_vec)
@@ -83,6 +111,8 @@ class Ball:
             normal_component_other*=0.8
             normal_component_self*=0.8
             
+            
+            # new formulas assuming masses are same and perfectly elastic
             self.vel.x=tangent_component_self*tangent_vec.x+normal_component_other*normal_vec.x
             self.vel.y=tangent_component_self*tangent_vec.y+normal_component_other*normal_vec.y
             other.vel.x=tangent_component_other*tangent_vec.y+normal_component_self*normal_vec.y
@@ -107,27 +137,36 @@ class Ball:
                 other.normal=False
             
     def display(self,screen:'pygame.Surface'):
+        '''displays the ball on the screen'''
         self.rect=pygame.draw.circle(screen,self.color,(self.pos.x,self.pos.y),self.radius)
 
 
-balls=[]
+balls=[] # list to store all the balls
 running=True
-while running:
+while running:#mainloop
+    
+    # event handling
     for event in pygame.event.get():
         if event.type==pygame.QUIT: running=False
         if(event.type==pygame.KEYDOWN):
-            if(event.key==pygame.K_SPACE):
+            if(event.key==pygame.K_SPACE):# space for 5 balls
                 for _ in range(5):
                     balls.append(Ball())
+                    
+    # background
     screen.fill("#1D1D1D")
+    
+    # ball handling
     for ball in balls:
         ball.update(clock.get_fps())
-        
         for other in balls:
             if ball!=other:
                 ball.collision(other)
         ball.display(screen)
+        
     # a simple fps counter
     screen.blit(pygame.font.Font(None, 30).render(f"FPS: {int(clock.get_fps())},    Balls: {len(balls)}", True, (255, 255, 255)), (10, 10))
+    
+    # update the display and tick the clock
     pygame.display.flip()
     clock.tick(fps)
--- physics/vector.py
+++ physics/vector.py
@@ -1,46 +1,79 @@
 import math
 
 class Vector:
+    '''a simple 2D vector class with some basic operations'''
+    
     def __init__(self,x=0,y=0):
+        '''initializes the vector with x and y components'''
         self.x=x
         self.y=y
+        
     def scaler(self):
+        '''returns the magnitude of the vector'''
         return (self.x**2+self.y**2)**0.5
+    
     def dot(self,other):
+        '''returns the dot product of self and other'''
         return self.x*other.x+self.y*other.y
+    
     def angle(self,other):
+        '''returns the angle between self and other in radians'''
         return math.acos(self.dot(other)/(self.scaler()*other.scaler()))
+    
     def __add__(self,other):
+        '''returns the sum of self and other'''
         return Vector(self.x+other.x,self.y+other.y)
+    
     def __sub__(self,other):
+        '''returns the difference of self and other'''
         return Vector(self.x-other.x,self.y-other.y)
+    
     def __mul__(self,scalar: float):
-        # vector multiplication by scalar
+        '''returns the product of self and scalar'''
         return Vector(self.x*scalar,self.y*scalar)
+    
     def __truediv__(self,scalar: float):
-        # vector division by scalar
+        '''returns the quotient of self and scalar'''
         return Vector(self.x/scalar,self.y/scalar)
+    
     def tangent(self):
+        '''returns the tangent vector of self'''
         return Vector(-self.y,self.x)
+    
     def unit(self):
-        # returs unit vector
+        '''returns the unit vector of self'''
         scaler=self.scaler()
         if scaler==0:
             return Vector(0,0)
         return Vector(self.x/scaler,self.y/scaler)
+    
     def roundoff(self,n=2):
-        
+        '''rounds off the components of the vector to n decimal places and also sets very small values to 0 to avoid jitter'''
         self.x=round(self.x,n)
+        if(abs(self.x)<0.05):
+            self.x=0
         self.y=round(self.y,n)
+        if(abs(self.y)<0.05):
+            self.y=0
     
 class position(Vector):
+    '''a simple position class that inherits from vector'''
+    
     def __init__(self,x=0,y=0):
+        '''initializes the position with x and y components'''
         super().__init__(x,y)
+        
 class velocity(Vector):
+    '''a simple velocity class that inherits from vector'''
+    
     def __init__(self,x=0,y=0):
+        '''initializes the velocity with x and y components'''
         super().__init__(x,y)
 class acceleration(Vector):
+    '''a simple acceleration class that inherits from vector'''
+    
     def __init__(self,x=0,y=0):
+        '''initializes the acceleration with x and y components'''
         super().__init__(x,y)
         
 
